24382a59afea0be9a94a113c944e4e91a084f6fe
TeoYM <S10262861@connect.np.edu.sg>
2023-11-30 22:02:37 +0800

    Buy function achieved

--- js.js
+++ js.js
@@ -1,9 +1,16 @@
 function buyProduct() {
-    alert("Product purchased! Thank you for shopping with us.");
-}
+    var name = prompt("Name: ");
+    var email = prompt("Email: ")
+    var houseAdd = prompt("House address: ");
+    var postalCode = prompt("Postal code: ");
+    var unit = prompt("Unit Number: ");
+    var creditCard = prompt("Credit Card Number:");
 
+    alert("Product purchased! Thank you, " + name + ", for shopping with us. \n\nConfirmation has been sent to " + email + ".\n\nProduct will be shipped to " + houseAdd + ", " + postalCode + ", " + unit + ".\n\nCredit Card No: " + creditCard);
+}
 var buyButtons = document.querySelectorAll(".buybutton");
 
 for (var button of buyButtons) {
     button.addEventListener("click", buyProduct);
 }
+        
